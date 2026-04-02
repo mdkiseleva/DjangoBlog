03c520fb08cf73bb812f27a342eede495e03a9a4
mdkiseleva <masha999m@gmail.com>
2026-04-03 01:27:44 +0300

    Add files via upload

---
+++ lab3/blog/articles/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .models import Article
+# Create your views here.
+def archive(request):
+    return render(request, 'archive.html', {"posts": Article.objects.all()})
